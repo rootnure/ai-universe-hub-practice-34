d09f4fddfa03a964674e5090773d7fb5e8fc5ea7
rootnure <nur.diu.2791@gmail.com>
2023-08-30 13:12:53 +0600

    tool cards more information added with not available image error handler

--- js/hub.js
+++ js/hub.js
@@ -9,26 +9,41 @@ const displayTools = tools => {
     const toolsContainer = document.getElementById('tools-container');
     tools.forEach(tool => {
         console.log(tool);
-        const {image, features} = tool;
+        const { image, features, name, published_in } = tool;
         const toolDiv = document.createElement('div');
         toolDiv.innerHTML = `
         <div class="card bg-base-100 shadow-xl">
             <figure class="px-10 pt-10">
-                <img src="${image}" alt="Shoes" class="rounded-xl" />
+                <img src="${image}" alt="${name}" class="rounded-xl" />
             </figure>
             <div class="card-body">
                 <h2 class="card-title">Features</h2>
                 <ol class="list-decimal list-inside">
-                    ${features.map(feature => `<li>${feature}</li>`).join(" ")}
+                ${features.map(feature => `<li>${feature}</li>`).join(" ")}
                 </ol>
-                <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
+                <hr>
+                <div class="flex justify-between">
+                    <div>
+                        <h2 class="text-lg font-bold">${name}</h2>
+                        <div>📅 <date date=${published_in}>${published_in}</date></div>
+                    </div>
+                    <button class="rounded-full p-2 bg-red-100 text-red-400 font-black">→</button>
                 </div>
             </div>
         </div>
         `;
         toolsContainer.appendChild(toolDiv);
     });
+    replaceUnavailableImages();
+}
+
+const replaceUnavailableImages = () => {
+    var images = document.getElementsByTagName('img');
+
+    for(const image of images) {
+        image.addEventListener('error', function () {
+        this.src = './images/loading.jpg';
+    })};
 }
 
 loadAll();
